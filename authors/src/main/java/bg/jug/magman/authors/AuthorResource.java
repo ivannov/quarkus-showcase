8c2b10948adb9d5c2935b3d0899dbaf3edfe1e6e
Gunnar Morling <gunnar.morling@googlemail.com>
2019-06-16 19:57:01 +0200

    Simplying sending of a Kafka message by using emitter

--- authors/src/main/java/bg/jug/magman/authors/AuthorResource.java
+++ authors/src/main/java/bg/jug/magman/authors/AuthorResource.java
@@ -1,39 +1,44 @@
 package bg.jug.magman.authors;
 
-import io.smallrye.reactive.messaging.kafka.KafkaMessage;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.*;
+
+import io.smallrye.reactive.messaging.annotations.Emitter;
+import io.smallrye.reactive.messaging.annotations.Stream;
+import io.smallrye.reactive.messaging.kafka.KafkaMessage;
 
 @Path("/authors")
 @ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
 public class AuthorResource {
 
+    private static final Jsonb JSONB = JsonbBuilder.create();
+
     private Map<String, Author> authors = new ConcurrentHashMap<>();
+
+    @Inject @Stream("authors") Emitter<KafkaMessage<String, String>> authorsEmitter;
 
     @GET
     public Collection<Author> listAll() {
         return authors.values();
     }
 
-    @Inject
-    private Event<Author> authorAddedEvent;
-
     @POST
     public Author addAuthor(Author author) {
         authors.put(author.email, author);
-        authorAddedEvent.fireAsync(author);
+        authorsEmitter.send(KafkaMessage.of("key", JSONB.toJson(author)));
         return authors.get(author.email);
     }
 
